ba8c6e40c7b0015cbe12d27e98c8175bb84e8feb
okcleff <okcleff@gmail.com>
2023-08-02 11:53:01 +0900

    온디맨드 선데이 아이스크림 앱 - 이용약관 호버 이벤트 테스트

--- sundae-client/src/pages/summary/test/SummaryForm.test.jsx
+++ sundae-client/src/pages/summary/test/SummaryForm.test.jsx
@@ -31,3 +31,24 @@ test('Checkbox enables button on the first click and disables on the second clic
   await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
+
+test('popover responds to hover', async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears on mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // popover disappears when we mouse out
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
+});
