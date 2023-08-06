a629a94b06353b144395468692183cc1a8d98456
woncheolok <okcleff@gmail.com>
2023-08-06 16:44:25 +0900

    온디맨드 선데이 아이스크림 앱 - 토핑 옵션 렌더링 테스트

--- sundae-client/src/App.js
+++ sundae-client/src/App.js
@@ -1,9 +1,12 @@
 import SummaryForm from './pages/summary/SummaryForm';
+import Options from './pages/entry/Options';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <SummaryForm />
+      <Options optionType='scoops' />
+      <Options optionType='toppings' />
     </div>
   );
 }
--- sundae-client/src/pages/entry/test/Options.test.js
+++ sundae-client/src/pages/entry/test/Options.test.js
@@ -16,3 +16,21 @@ test('displays an image for each scoop option from server', async () => {
   // 배열이나 객체는 toEqual 메소드를 사용
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 });
+
+test('Displays image for each topping option from server', async () => {
+  // Mock Service Worker will return three toppings from server
+  render(<Options optionType='toppings' />);
+
+  // find images, expect 3 based on what msw returns
+  const images = await screen.findAllByRole('img', { name: /topping$/i });
+  expect(images).toHaveLength(3);
+
+  // check the actual alt text for the images
+  // @ts-ignore
+  const imageTitles = images.map((img) => img.alt);
+  expect(imageTitles).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
+  ]);
+});
